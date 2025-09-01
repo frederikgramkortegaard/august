c967a95435b266062877beda35b1fdbc01f9c4ef
Frederik Gram Kortegaard <fgk@Frederiks-MacBook-Pro.local>
2025-09-01 11:12:00 +0400

    Fix deployment script deploying 1 more node than expected

--- deploy_nodes.sh
+++ deploy_nodes.sh
@@ -4,7 +4,7 @@ num=$1  # first argument
 
 trap "kill 0" EXIT
     go run ./cmd/main.go -p2p "9360" &
-for ((i=0; i<num; i++)); do
+for ((i=0; i<num-1; i++)); do
   go run ./cmd/main.go -p2p "937${i}" -seeds 0.0.0.0:9360 &
 done
 wait
